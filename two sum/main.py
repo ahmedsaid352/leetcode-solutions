a12ff73307651830ca2b8ccec08bfaf555970bac
Ahmed said <ahmedsaid2000321@gmail.com>
2023-02-09 15:13:30 +0200

    two sum II

---
+++ two sum/main.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums[i+1:])):
+                if (nums[i] + nums[j+i+1] == target):
+                    res = [i,j+i+1]
+                    break
+        return res
