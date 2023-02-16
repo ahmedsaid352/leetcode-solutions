4561bda75b2b3e38afb0bd4f5b1a6d3052dc38dd
Ahmed said <ahmedsaid2000321@gmail.com>
2023-02-16 15:53:32 +0200

    max-area-of-island II

--- max-area-of-island/main.py
+++ max-area-of-island/main.py
@@ -4,25 +4,21 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        areas =[]
-        ones = []
+        max_area = 0
+        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    ones.append((i,j))
-        if len(ones) > 0:
-            for one in ones:
-                current_area = 0
-                frontier = [one]
-                visited = set()
-                while len(frontier) > 0:
-                    r,c = frontier.pop()
-                    visited.add((r,c))
-                    current_area+=1
-                    for r,c in [(r,c) for r,c in [(r-1,c) , (r+1,c),(r,c-1),(r,c+1)] if (len(grid) > r >=0 and len(grid[0]) > c >= 0 ) and grid[r][c] == 1]:
-                        if (r,c) not in visited:
-                            frontier.append((r,c))
-                areas.append(current_area)
-            return max(areas)
-        else:
-            return 0
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    current_area = 0
+                    frontier = [(i,j)]
+                    visited.add((i,j))
+                    while frontier:
+                        r,c = frontier.pop()
+                        current_area+=1
+                        for r, c in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+                            if (len(grid) > r >=0 and len(grid[0]) > c >= 0 ) and (grid[r][c] == 1) and ((r,c) not in visited):
+                                frontier.append((r,c))
+                                visited.add((r,c))
+                    if max_area < current_area:
+                        max_area = current_area
+        return max_area
